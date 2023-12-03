e11f10789b874599291db6155e5bc8cb4dbe28e5
TechWithJin <130134152+TechWithJin@users.noreply.github.com>
2023-12-03 12:37:15 +0900

    Update Todo.js

--- frontend/src/componenets/Todo.js
+++ frontend/src/componenets/Todo.js
@@ -8,7 +8,7 @@ function TodoItem(props) {
             return (
                 <div>
                     <p>
-                        <span style={{fontWeight:'bold, underline'}}>{props.todo.title} : </span> {props.todo.description}
+                        <span style={{fontWeight:'bold, underline', color:'white'}}>{props.todo.title} : </span> {props.todo.description}
                         <button onClick={() => deleteTodoHandler(props.todo.title)} className="btn btn-outline-danger my-2 mx-2" style={{'borderRadius':'50px',}}>X</button>
                         <hr></hr>
                     </p>
